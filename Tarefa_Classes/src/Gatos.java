aaa9a9173865d6e2fd7e54c8c78a018f794dc260
Gabriel Batista <121689888+gabatist4@users.noreply.github.com>
2024-02-18 12:23:44 -0300

    Add files via upload

---
+++ Tarefa_Classes/src/Gatos.java
@@ -0,0 +1,38 @@
+/**
+ * @author Gabriel Batista
+ */
+public class Gatos {
+
+    private int codigo;
+
+    private String nome;
+
+    private String raca;
+
+    public int getCodigo() {
+        return 1;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return "Dexter";
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return "Vira-lata";
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+}
+
+
---
+++ Tarefa_Classes/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main (String args[]) {
+
+        Gatos gatos = new Gatos();
+        gatos.getCodigo();
+        System.out.println(gatos.getCodigo());
+        gatos.getNome();
+        System.out.println(gatos.getNome());
+        gatos.getRaca();
+        System.out.println(gatos.getRaca());
+    }
+}
