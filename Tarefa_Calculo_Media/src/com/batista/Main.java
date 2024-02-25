23cd4e268cc067bfe57f5afb9ca79d64e454eb6f
Gabriel Batista <121689888+gabatist4@users.noreply.github.com>
2024-02-25 19:30:25 -0300

    Add files via upload

---
+++ Tarefa_Calculo_Media/src/com/batista/Main.java
@@ -0,0 +1,14 @@
+package com.batista;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int nota1 = 10;
+            int nota2 = 8;
+            int nota3 = 6;
+            int nota4 = 10;
+
+            int media = (nota1 + nota2 + nota3 + nota4) / 4;
+            System.out.println("Media: " + media);
+    }
+}
