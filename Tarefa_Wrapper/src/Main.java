dee9770857a1778804e37b58ecd7c5ce5b48084e
Gabriel Batista <121689888+gabatist4@users.noreply.github.com>
2024-02-26 21:56:13 -0300

    Add files via upload

---
+++ Tarefa_Wrapper/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        float saldo = 10000;
+        System.out.println(saldo);
+        long saldo1 = (long) saldo;
+        System.out.println(saldo1);
+    }
+}
