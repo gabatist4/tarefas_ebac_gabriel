bd7c18f2c7749b671ece35570a2d2033215efc73
Gabriel Batista <121689888+gabatist4@users.noreply.github.com>
2024-03-25 22:14:28 -0300

    Add files via upload

---
+++ Tarefa_Annotations/src/AnotacaoTabela.java
@@ -0,0 +1,12 @@
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD})
+public @interface AnotacaoTabela {
+    String value();
+
+    String[] apartamento();
+
+    int numeroBloco();
+}
---
+++ Tarefa_Annotations/src/Tabela.java
@@ -0,0 +1,6 @@
+@AnotacaoTabela(value = "Condomínio", apartamento = {"AP 11", "AP 12", "AP 13", "AP 14"}, numeroBloco = 300)
+public class Tabela {
+
+    @AnotacaoTabela(value = "Condomínio", apartamento = {"AP 11", "AP 12", "AP 13", "AP 14"}, numeroBloco = 300)
+    private String nome;
+}
