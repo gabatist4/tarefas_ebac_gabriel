f51c403abcbcf624025b504a9bac33fd23b0c303
Gabriel Batista <121689888+gabatist4@users.noreply.github.com>
2024-04-14 14:54:50 -0300

    Add files via upload

---
+++ CadastroCliente/src/com/batista/dao/IClienteDAO.java
@@ -0,0 +1,16 @@
+package com.batista.dao;
+
+import com.batista.domain.Cliente;
+import java.util.Collection;
+
+public interface IClienteDAO {
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
