a5930e07b3fd432fc79b720dc698febb7bf5ed4e
Gabriel Batista <121689888+gabatist4@users.noreply.github.com>
2024-04-16 20:24:30 -0300

    Add files via upload

---
+++ Porjeto2_mod25/ExemploVendasMod25/src/main/java/com/batista/dao/ClienteDAO.java
@@ -0,0 +1,31 @@
+package com.batista.dao;
+
+import com.batista.dao.generic.GenericDAO;
+import com.batista.domain.Cliente;
+
+public class ClienteDAO extends GenericDAO<Cliente, Long> implements IClienteDAO {
+
+	public ClienteDAO() {
+		super();
+	}
+
+	@Override
+	public Class<Cliente> getTipoClasse() {
+		return Cliente.class;
+	}
+
+	@Override
+	public void atualiarDados(Cliente entity, Cliente entityCadastrado) {
+		entityCadastrado.setCidade(entity.getCidade());
+		entityCadastrado.setCpf(entity.getCpf());
+		entityCadastrado.setEnd(entity.getEnd());
+		entityCadastrado.setEstado(entity.getEstado());
+		entityCadastrado.setNome(entity.getNome());
+		entityCadastrado.setNumero(entity.getNumero());
+		entityCadastrado.setTel(entity.getTel());
+		
+	}
+
+
+
+}
---
+++ Porjeto2_mod25/ExemploVendasMod25/src/main/java/com/batista/dao/Persistente.java
@@ -0,0 +1,8 @@
+package com.batista.dao;
+
+ //Classe que representa todas as entidades ou objetos da aplicação que seram salvas no banco de dados
+
+public interface Persistente {
+
+    //public Long getCodigo();
+}
